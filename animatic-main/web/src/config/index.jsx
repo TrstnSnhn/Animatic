bcbf6b384856ffef5b7f0d250fbe29230530632b
Tristan Sinohin <121960353+TrstnSnhn@users.noreply.github.com>
2026-01-27 18:14:06 +0000

    index changes

--- animatic-main/web/src/config/index.jsx
+++ animatic-main/web/src/config/index.jsx
@@ -1,3 +1,5 @@
 const getBackendURL = () => {
-    return import.meta.env.VITE_API_URL || "https://trstnsnhn-animatics.hf.space";
-}
+  return import.meta.env.VITE_API_URL || "https://trstnsnhn-animatics.hf.space";
+};
+
+export default getBackendURL;
